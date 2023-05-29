bc1d037a09c0f8e668fc21423989ff883a8231ca
Akhlaq Ahmed <akhlaqahmed990@gmail.com>
2023-05-30 02:46:43 +0500

    day 04 done

--- day 04.js
+++ day 04.js
@@ -21,3 +21,53 @@ function getSecondLargest(nums) {
     
     return secondLargest;
 }
+
+
+// task 02
+// retrun an object
+
+function Rectangle(a, b) {
+    return {
+        length: a,
+        width: b,
+        perimeter: (a+b)*2,
+        area: a*b
+    }
+}
+
+
+// task 03
+// iteratin over array of objects
+
+function getCount(objects) {
+    let count = 0;
+    objects.forEach((o, i) => {
+        if (o.x === o.y){
+            count++;
+        }
+    })
+    
+    return count;
+}  
+
+
+
+// task 04
+
+// basic classes
+
+class Polygon{
+    constructor(arr){
+        this.sides = arr;
+    }
+    
+    perimeter () {
+        let peri = 0
+        
+        for (let i = 0; i < (this.sides).length; i++){
+            peri += this.sides[i]
+        }
+        
+        return peri;
+    }
+}
