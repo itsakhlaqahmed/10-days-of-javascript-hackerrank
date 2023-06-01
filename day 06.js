435040f9e3abde845fef58e5204812c80c95779f
Akhlaq Ahmed <akhlaqahmed990@gmail.com>
2023-06-02 01:48:52 +0500

    day 6 task 2 done

--- day 06.js
+++ day 06.js
@@ -14,3 +14,21 @@ function regexVar() {
      */
     return re;
 }
+
+
+
+// task 02 
+// regex to capture all numbers in a string 
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+    
+    let re = /\D+/g
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
